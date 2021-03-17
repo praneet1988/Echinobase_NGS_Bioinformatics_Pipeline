b8b07e26ac346694b85eb1dbe92f7188a9404309
Praneet Chaturvedi <55851416+praneet1988@users.noreply.github.com>
2021-03-17 17:50:54 -0400

    Add files via upload

--- bin/Download_TechnicalReps.sh
+++ bin/Download_TechnicalReps.sh
@@ -7,7 +7,7 @@ PREFETCH=$3
 FASTQDUMP=$4
 SRAPATH=$5
 
-$PREFETCH $SAMPLE
+$PREFETCH --max-size 1000G $SAMPLE
 mv $SRAPATH/$SAMPLE.sra $DATADIR
 $FASTQDUMP --split-3 -O $DATADIR $DATADIR/$SAMPLE.sra
 echo "Downloaded Technical Rep $SAMPLE from SRA and converted to fastq"
